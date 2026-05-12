060a139ee47d3657c9600a0d289018a9fdae270f
Sid <sahani.bunty9@gmail.com>
2026-05-12 15:41:31 +0530

    Added Read For Later Functionality

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("", views.StartPageView.as_view(), name="starting_page"),
     path("posts/", views.AllPostsView.as_view(), name="all-posts"),
     path("posts/<slug:slug>", views.SinglePostView.as_view(), name="post_detail_page"),
+    path("read-later/", views.ReadLaterView.as_view(), name="read_later"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from datetime import date, datetime
 from tokenize import Single
 from turtle import pos
 from django.shortcuts import render
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 
 from django.views.generic import ListView
 from django.views.generic.detail import DetailView
@@ -44,13 +44,24 @@ class AllPostsView(ListView):
 
 
 class SinglePostView(View):
+    def is_stored_post(self, request, post_id):
+        stored_posts = request.session.get("stored_posts")
+        if stored_posts is not None:
+            is_saved_for_later = post_id in stored_posts
+        else:
+            is_saved_for_later = False
+
+        return is_saved_for_later
+
     def get(self, request, slug):
         post = Post.objects.get(slug=slug)
+
         context = {
             "post": post,
             "all_tags": post.tags.all(),
             "comments": post.comments.all().order_by("-id"),
             "comment_form": CommentForm(),
+            "is_saved_for_later": self.is_stored_post(request, post.id),
         }
         return render(request, "blog/post-detail.html", context)
 
@@ -61,6 +72,7 @@ class SinglePostView(View):
             "all_tags": post.tags.all(),
             "comments": post.comments.all().order_by("-id"),
             "comment_form": CommentForm(),
+            "is_saved_for_later": self.is_stored_post(request, post.id),
         }
         comment_form = CommentForm(request.POST)
         if comment_form.is_valid():
@@ -68,3 +80,33 @@ class SinglePostView(View):
             comment.post = post
             comment.save()
         return render(request, "blog/post-detail.html", context)
+
+
+class ReadLaterView(View):
+    def get(self, request):
+        stored_posts = request.session.get("stored_posts")
+        context = {}
+        if stored_posts is None or len(stored_posts) == 0:
+            context["posts"] = []
+            context["has_posts"] = False
+        else:
+            posts = Post.objects.filter(id__in=stored_posts)
+            context["posts"] = posts
+            context["has_posts"] = True
+        return render(request, "blog/read-later.html", context)
+
+    def post(self, request):
+        stored_posts = request.session.get("stored_posts")
+        if stored_posts is None:
+            stored_posts = []
+
+        post_id = int(request.POST["post_id"])
+
+        if post_id not in stored_posts:
+            stored_posts.append(post_id)
+        else:
+            stored_posts.remove(post_id)
+
+        request.session["stored_posts"] = stored_posts
+
+        return HttpResponseRedirect("/")
